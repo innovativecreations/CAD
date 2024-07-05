946e4a2191fd613d39748767872c52c9ecde80ca
Mayank Kumar <52621155+innovativecreations@users.noreply.github.com>
2024-07-05 18:54:05 +0530

    complete the viewer

--- final/server.js
+++ final/server.js
@@ -10,6 +10,8 @@ app.use('/cad', express.static(path.join(__dirname, 'public', 'cad')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
+app.get('/viewer', (req, res) => res.sendFile(path.join(__dirname, 'public', 'viewer.html')));
+
 app.get('/models', (req, res) => {
     const basePath = path.join(__dirname, 'public', 'cad');
     fs.readdir(basePath, (err, folders) => {
