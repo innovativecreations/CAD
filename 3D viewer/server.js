9898735ea14abfd5debef63a9e8aa657f4eddb70
Mayank <innovativecreations195@gmail.com>
2024-07-05 14:04:13 +0530

    add zoom buttons

--- 3D viewer/server.js
+++ 3D viewer/server.js
@@ -10,4 +10,3 @@ app.use('/cad', express.static(path.join(__dirname, 'public', 'cad')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
